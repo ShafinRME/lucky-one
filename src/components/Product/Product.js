40d88336927e77819b11f0af24ecf47e0aa43d5e
ShafinRME <shafin.rmedu4@gmail.com>
2022-03-26 01:45:22 +0600

    Alhamdulillah.Media Query Done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css'
 
-const Product = ({ product }) => {
+const Product = ({ product, handleAddToCart }) => {
     const { name, img, price, id } = product;
     return (
         <div className='product'>
@@ -9,8 +11,8 @@ const Product = ({ product }) => {
             <p className='product-name'>{name}</p>
             <p className='product-price'>{price}</p>
             <div>
-                <button className='btn-cart'>
-                    ADD TO CART
+                <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                    <p>ADD TO CART <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon> </p>
                 </button>
             </div>
         </div>
