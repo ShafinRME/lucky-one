ea9b20198da0151ffd006517b8c82de2386e08ea
ShafinRME <shafin.rmedu4@gmail.com>
2022-03-25 22:36:46 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='main-header'>
+            <p className='header-text-1'>Phone Bazar</p>
+            <p className='header-text-2'>Choose 4 Phones!!!</p>
+        </nav>
+    );
+};
+
+export default Header;
